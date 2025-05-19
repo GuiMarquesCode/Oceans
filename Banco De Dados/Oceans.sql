b7201fde3e75e4ad260fac2b2de16f86f5c2dabf
GuiMarquesCode <guilherme.csantos@sptech.school>
2025-05-18 21:56:26 -0300

    Ajustando selects do banco de dados

--- Banco De Dados/Oceans.sql
+++ Banco De Dados/Oceans.sql
@@ -87,15 +87,20 @@ INSERT INTO Postagem VALUES
 SELECT Musica.Titulo as Titulo, Album.Titulo_Album as Nome_Album, Album.Data_lancamento as Lancamento, Artista.Nome as Artista 
 FROM Musica JOIN Album ON Musica.FkAlbum= Album.idAlbum JOIN Artista ON Album.FkArtista = Artista.idArtista;
 
-SELECT Postagem.Sentimento as Sentimento, Postagem.Historia AS Historia, Musica.Titulo AS Musica , Album.foto as Foto, Musica.Preview
+SELECT Postagem.Sentimento as Sentimento, Artista.Nome as Artista ,Postagem.Historia AS Historia, Musica.Titulo AS Musica, 
+Album.foto as Foto, Musica.Preview,  DATE_FORMAT(Postagem.DataPostagem, '%d/%m/%Y %H:%i') as "Data"
 FROM Postagem JOIN Usuario ON Postagem.FkUsuario = Usuario.idUsuario JOIN Musica ON Postagem.FkMusica = Musica.idMusica 
-JOIN Album ON Musica.FkAlbum = Album.idAlbum WHERE Usuario.idUsuario = 1;
+JOIN Album ON Musica.FkAlbum = Album.idAlbum JOIN Artista ON Album.FkArtista = Artista.idArtista WHERE Usuario.idUsuario = 1;
 
 	
 -- Feed
-SELECT Usuario.idUsuario as ID,Usuario.Nome as Nome, Postagem.Sentimento as Sentimento, Postagem.Historia AS Historia, Postagem.DataPostagem as Data,Musica.Titulo AS Musica , Album.foto as Foto, Musica.Preview
+SELECT Usuario.idUsuario as ID,Usuario.Nome as Nome, Postagem.Sentimento as Sentimento, Postagem.Historia AS Historia,
+  DATE_FORMAT(Postagem.DataPostagem, '%d/%m/%Y %H:%i') as "Data",Musica.Titulo AS Musica , Album.foto as Foto,
+Musica.Preview, Artista.Nome as Artista
 FROM Postagem JOIN Usuario ON Postagem.FkUsuario = Usuario.idUsuario JOIN Musica ON Postagem.FkMusica = Musica.idMusica 
-JOIN Album ON Musica.FkAlbum = Album.idAlbum WHERE Usuario.idUsuario ORDER BY Postagem.DataPostagem DESC;
+JOIN Album ON Musica.FkAlbum = Album.idAlbum
+JOIN Artista ON Album.FkArtista = Artista.idArtista
+WHERE Usuario.idUsuario ORDER BY Postagem.DataPostagem DESC;
 
 -- Estatistiscas artista	
 SELECT COUNT(Postagem.Sentimento) AS Num_Post FROM Postagem JOIN Musica ON Postagem.FkMusica = Musica.idMusica
@@ -104,6 +109,66 @@ JOIN Album ON Musica.FkAlbum = Album.idAlbum JOIN Artista ON Album.FkArtista = A
 -- Pegar Ranking
  SELECT Artista.idArtista ,Artista.Nome, Artista.Foto, COUNT(Postagem.Sentimento) AS Num_Post FROM Postagem JOIN Musica ON Postagem.FkMusica = Musica.idMusica
 JOIN Album ON Musica.FkAlbum = Album.idAlbum JOIN Artista ON Album.FkArtista = Artista.idArtista
-GROUP BY Artista.idArtista ,Artista.Nome, Artista.Foto ORDER BY COUNT(Postagem.idPostagem) DESC LIMIT 3;   
+GROUP BY Artista.idArtista ,Artista.Nome, Artista.Foto ORDER BY COUNT(Postagem.idPostagem) DESC LIMIT 3; 
+
+-- Selecionar maior sentimento de cada artista
+
+SELECT Artista.Nome AS Nome, Postagem.Sentimento , Count(Postagem.Sentimento) FROM Artista
+JOIN Album ON Album.FkArtista = Artista.idArtista 
+JOIN Musica ON Musica.FkAlbum = Album.idAlbum
+JOIN Postagem ON Postagem.FkMusica = Musica.idMusica GROUP BY Artista.Nome,Postagem.Sentimento ;  
     
-    
+-- Principais sentimentos
+
+SELECT Postagem.Sentimento AS Sentimento, Count(Postagem.Sentimento) as Quantidade 
+FROM Postagem JOIN Musica ON Postagem.FkMusica = Musica.idMusica 
+JOIN Album ON Musica.FkAlbum = Album.idAlbum
+JOIN Artista ON Album.FkArtista = Artista.idArtista
+WHERE Artista.idArtista = 4495513 GROUP BY Postagem.Sentimento ORDER BY Count(Postagem.Sentimento) Desc;
+
+-- Mostrar as 3 músicas mais cadastradas de cada artista
+
+SELECT Musica.Titulo as Titulo, Count(Postagem.FkMusica) as "Quantidade"
+FROM  Postagem JOIN Musica ON Postagem.FkMusica = Musica.idMusica
+JOIN Album ON Musica.FkAlbum = Album.idAlbum JOIN Artista ON 
+Album.FkArtista = Artista.idArtista WHERE Artista.idArtista = 4495513
+GROUP BY Musica.Titulo , Postagem.FkMusica ORDER BY Count(Postagem.FkMusica) DESC limit 3;
+
+
+-- Mostrar o total de postagens do usuário
+
+SELECT Usuario.Nome as Nome, COUNT(Postagem.FkUsuario) FROM 
+Usuario JOIN Postagem ON Postagem.FkUsuario = Usuario.idUsuario where idUsuario = 5;
+
+-- Mostrar o maior sentimento que o usuário sentiu 
+
+SELECT Postagem.Sentimento as Sentimento,
+COUNT(Postagem.Sentimento) as Sentimentos FROM Usuario 
+JOIN Postagem ON Postagem.FkUsuario = Usuario.idUsuario WHERE idUsuario = 5
+GROUP BY Postagem.Sentimento ORDER BY COUNT(Postagem.Sentimento) Desc LIMIT 1;
+
+-- lista a quantidade de cada sentimentos que o usuario sentiu alter
+
+SELECT  Postagem.Sentimento as Sentimento,
+COUNT(Postagem.Sentimento) as Sentimentos FROM Usuario 
+JOIN Postagem ON Postagem.FkUsuario = Usuario.idUsuario WHERE idUsuario = 5
+GROUP BY Postagem.Sentimento ORDER BY COUNT(Postagem.Sentimento) Desc;
+
+-- Artista favorito do usuário
+
+SELECT 
+  Usuario.Nome AS Nome, 
+  Artista.Nome AS Artista,
+  COUNT(Postagem.idPostagem) AS Numero
+FROM Usuario
+JOIN Postagem ON Postagem.FkUsuario = Usuario.idUsuario
+JOIN Musica ON Postagem.FkMusica = Musica.idMusica
+JOIN Album ON Musica.FkAlbum = Album.idAlbum
+JOIN Artista ON Album.FkArtista = Artista.idArtista
+WHERE Usuario.idUsuario = 1
+GROUP BY Usuario.Nome, Artista.Nome
+ORDER BY COUNT(Postagem.idPostagem) DESC limit 1;
+
+
+
+
